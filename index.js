743cfddb61bdb0b11b9095b4c0c43af65d4c8b17
Autumn Fjeld <autumnfjeld@gmail.com>
2017-12-20 11:32:21 +1100

    Setup chat groups

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var express = require('express'),
     cors = require('cors'),
     io = require('socket.io');
 
+// TODOs
+// Detect unread messages
+// Save messages and send unread
+// Add cookies
+// 
+
 // Setup server
 var app = express()
     server = http.createServer(app),
@@ -13,23 +19,28 @@ app.set('port', (process.env.PORT || 5000));
 
 // Listen for requests
 server.listen(app.get('port'), function () {
-  console.log('tincantelephone api listening on', app.get('port'))   //check on localhost:3000/api/routexxx
+  console.log('tincantelephone api listening on', app.get('port'))   
 });
 
 // Setup socket.io
 socketIo.on('connection', function(socket) {
-    var username = socket.handshake.query.username;
-    console.log(`${username} connected`);
+    var username = socket.handshake.query.username,
+        group = socket.handshake.query.group,
+        message = socket.handshake.query.message;
 
+    console.log(`\nusername:${username} in group:${group} connected`);
+    
+    socket.join(group);
     socket.on('client:message', function(data) {
         // Message received from client. 
-        console.log(`${data.username}: ${data.message}`);
+        console.log(`${data.username} in ${group}: ${data.message}`);
 
-        // Broadcast message to everyone else
-        socket.broadcast.emit('server:message', data);
-    });
+        // Broadcast message to everyone in the group
+        socket.to(group).emit('server:message', data);
+    });        
+
+
     // Log user disconnection
-
     socket.on('disconnect', function() {
         console.log(`${username} disconnected`);
     });
