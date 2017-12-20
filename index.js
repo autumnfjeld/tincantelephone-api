073cf71340383d55c369b5606d5e8f312686605a
Autumn Fjeld <autumnfjeld@gmail.com>
2017-12-20 15:56:09 +1100

    Add quick and dirty messageStore

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var express = require('express'),
 // Detect unread messages
 // Save messages and send unread
 // Add cookies
-// 
+
+// Quick & dirty message store
+var messageStore = {test: 'hi'};
 
 // Setup server
 var app = express()
@@ -19,7 +21,7 @@ app.set('port', (process.env.PORT || 5000));
 
 // Listen for requests
 server.listen(app.get('port'), function () {
-  console.log('tincantelephone api listening on', app.get('port'))   
+  console.log('tincantelephone api listening on', app.get('port'));
 });
 
 // Setup socket.io
@@ -28,17 +30,23 @@ socketIo.on('connection', function(socket) {
         group = socket.handshake.query.group,
         message = socket.handshake.query.message;
 
-    console.log(`\nusername:${username} in group:${group} connected`);
+    console.log('test', messageStore.test);
+    messageStore[group] = [];
+
+    
+    console.log(`\nusername:${username} in ${group} connected`);
     
     socket.join(group);
     socket.on('client:message', function(data) {
         // Message received from client. 
-        console.log(`${data.username} in ${group}: ${data.message}`);
+        console.log(`${data.username} in ${data.group} at ${data.timeStamp}: ${data.message}`);
+        // console.log('data', data);
+        messageStore[data.group].push(data);
+        console.log(group, ' messageStore count', messageStore[group].length);
 
         // Broadcast message to everyone in the group
         socket.to(group).emit('server:message', data);
     });        
-
 
     // Log user disconnection
     socket.on('disconnect', function() {
