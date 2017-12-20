ec2b56025e3f0f99ca2421cf80d5185f32a10529
Autumn Fjeld <autumnfjeld@gmail.com>
2017-12-21 08:13:38 +1100

    Send past messages after joining a group

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var express = require('express'),
     http = require('http'),
     cors = require('cors'),
-    io = require('socket.io');
+    io = require('socket.io'),
+    moment = require('moment');
 
 // TODOs
+// Refactor in ES6
+// Read up on structing a node app
 // Detect unread messages
 // Save messages and send unread
 // Add cookies
 
 // Quick & dirty message store
-var messageStore = {test: 'hi'};
+var messageStore = {test: 'hi'},
+    newId;
 
 // Setup server
 var app = express()
@@ -28,18 +32,39 @@ server.listen(app.get('port'), function () {
 socketIo.on('connection', function(socket) {
     var username = socket.handshake.query.username,
         group = socket.handshake.query.group,
-        message = socket.handshake.query.message;
+        message = socket.handshake.query.message,
+        newMessage = {
+            username: 'God',
+            message: username + ' joined the ' + group + 'group',
+            unixTimeStamp: moment().unix()
+        },
+        welcomeMessage = {
+            username: 'God',
+            message: 'Hello. Tell me everything.',
+            unixTimeStamp: moment().unix()
+        },        
+        timeStamp;
+    // console.log('socket', socket);
 
-    console.log('test', messageStore.test);
-    messageStore[group] = [];
+    messageStore[group] = messageStore[group] || [];
+    console.log(group, ' messageStore count', messageStore[group].length);
+    console.log(`\nNew connection: username:${username} id:${socket.id}`);
 
-    
-    console.log(`\nusername:${username} in ${group} connected`);
-    
-    socket.join(group);
+    // Join the group
+    socket.join(group, function() {
+        console.log(`Joined ${group}: username:${username}`);
+        // Send past messages to client
+        socketIo.to(socket.id).emit('server:pastMessages', messageStore[group]);
+        socketIo.to(socket.id).emit('server:message',welcomeMessage);
+    });
+
+    // Announce new person, send to everyone except new person
+    socket.to(group).emit('server:message', newMessage);
+
+    // Listen for message received from client. 
     socket.on('client:message', function(data) {
-        // Message received from client. 
-        console.log(`${data.username} in ${data.group} at ${data.unixTimeStamp}: ${data.message}`);
+        timeStamp = moment.unix(data.unixTimeStamp).format();
+        console.log(`${data.username} in ${data.group} at ${timeStamp}: ${data.message}`);
         // console.log('data', data);
         messageStore[data.group].push(data);
         console.log(group, ' messageStore count', messageStore[group].length);
